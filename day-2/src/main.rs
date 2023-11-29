7690ba7bac7cc070a9a9b1c62e74a2192062fd2c
Mitesh savaliya <miteshsavaliya7@gmail.com>
2023-11-29 16:49:07 +0530

    day-2 part-2 in process

--- day-2/src/main.rs
+++ day-2/src/main.rs
@@ -33,6 +33,34 @@ impl Outcome {
 }
 
 #[derive(Debug, Clone)]
+enum Expectation {
+    Win,
+    Draw,
+    Lose,
+}
+impl Expectation {
+    fn value(&self, other: &Move) -> u32 {
+        match self {
+            Expectation::Win => match other {
+                Move::Rock => 6,
+                Move::Paper => 0,
+                Move::Scissors => 3,
+            },
+            Expectation::Draw => match other {
+                Move::Rock => 3,
+                Move::Paper => 1,
+                Move::Scissors => 2,
+            },
+            Expectation::Lose => match other {
+                Move::Rock => 0,
+                Move::Paper => 2,
+                Move::Scissors => 1,
+            },
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
 enum Round {
     RockPaper,
     RockScissors,
@@ -59,24 +87,33 @@ fn main() {
     file.read_to_string(&mut contents).unwrap();
     let lines: Vec<&str> = contents.lines().collect();
     let mut total_score = 0;
+    let mut total_value_future = 0;
     for line in lines {
         let parts: Vec<&str> = line.split(" ").collect();
         let my_move = parts[1];
         let oppent_move = parts[0];
-        let my_move: Move = match my_move {
+        let my_move2: Move = match my_move {
             "X" => Move::Rock,
             "Y" => Move::Paper,
             "Z" => Move::Scissors,
             _ => panic!("invalid move"),
         };
-        let my_move_value = my_move.value();
+        let my_move_value = my_move2.value();
+        let my_move_future = match my_move {
+            "X" => Expectation::Lose,
+            "Y" => Expectation::Draw,
+            "Z" => Expectation::Win,
+            _ => panic!("invalid move"),
+        };
         let oppent_move: Move = match oppent_move {
             "A" => Move::Rock,
             "B" => Move::Paper,
             "C" => Move::Scissors,
             _ => panic!("invalid move"),
         };
-        let outcome = match (my_move, oppent_move) {
+        let my_move_value_future = my_move_future.value(&oppent_move);
+        total_value_future += my_move_value_future;
+        let outcome = match (my_move2, oppent_move) {
             (Move::Rock, Move::Rock) => Outcome::Draw,
             (Move::Paper, Move::Paper) => Outcome::Draw,
             (Move::Scissors, Move::Scissors) => Outcome::Draw,
@@ -87,7 +124,9 @@ fn main() {
             (Move::Paper, Move::Rock) => Outcome::Win,
             (Move::Scissors, Move::Paper) => Outcome::Win,
         };
+        // let outcome_new_str = match
         total_score += outcome.value() + my_move_value;
     }
     println!(" total score: {}", total_score);
+    println!(" total value future: {}", total_value_future);
 }
