67b96dd3abb1e9ab53b038f18e87c7badba2dd1d
Mitesh savaliya <miteshsavaliya7@gmail.com>
2023-11-29 11:20:30 +0530

    day-1 part-2

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -9,16 +9,29 @@ fn main() {
             let v: Vec<&str> = contents.split("\n").collect();
             let mut highest_calories = 0;
             let mut total_calories_per_elf = 0;
+            let mut top_three_calories = [0, 0, 0];
+            let mut u32_vec: Vec<u32> = Vec::new();
             for i in 0..v.len() {
                 if v[i] == "" {
                     if total_calories_per_elf > highest_calories {
                         highest_calories = total_calories_per_elf;
+                        top_three_calories[2] = top_three_calories[1];
+                        top_three_calories[1] = top_three_calories[0];
+                        top_three_calories[0] = total_calories_per_elf;
+                    } else if total_calories_per_elf > top_three_calories[2] {
+                        top_three_calories[2] = total_calories_per_elf;
+                    } else if total_calories_per_elf > top_three_calories[1] {
+                        top_three_calories[2] = top_three_calories[1];
+                        top_three_calories[1] = total_calories_per_elf;
                     }
                     total_calories_per_elf = 0;
                 } else {
+                    u32_vec.push(v[i].parse::<u32>().unwrap());
                     total_calories_per_elf += v[i].parse::<u32>().unwrap();
                 }
             }
+            let top_three_calories_sum: u32 = top_three_calories.iter().sum();
+            println!("{}", top_three_calories_sum);
             println!("{}", highest_calories);
         }
         Err(e) => {
