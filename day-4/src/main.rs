19ce09290659b93d358f39d7c8dd66b2029b2c0d
Mitesh savaliya <miteshsavaliya7@gmail.com>
2023-11-30 13:32:36 +0530

    day-4 part-1 solution

--- day-4/src/main.rs
+++ day-4/src/main.rs
@@ -1,4 +1,3 @@
-use std::collections::HashSet;
 use std::fs::File;
 use std::io::prelude::*;
 fn main() {
@@ -7,5 +6,27 @@ fn main() {
     file.read_to_string(&mut contents).unwrap();
     let lines: Vec<&str> = contents.lines().collect();
     let length = lines.len();
-    println!(" length: {}", length);
+    let mut count = 0;
+    for i in 0..length {
+        let line = lines[i];
+        let parts: Vec<&str> = line.split(",").collect();
+        let part1 = parts[0];
+        let part2 = parts[1];
+        let range_of_first_elf: Vec<&str> = part1.split("-").collect();
+        let range_of_second_elf: Vec<&str> = part2.split("-").collect();
+        let first_elf_start = range_of_first_elf[0].parse::<u32>().unwrap();
+        let first_elf_end = range_of_first_elf[1].parse::<u32>().unwrap();
+        let second_elf_start = range_of_second_elf[0].parse::<u32>().unwrap();
+        let second_elf_end = range_of_second_elf[1].parse::<u32>().unwrap();
+        // 7-10 , 11-13
+        // 11-13 , 7-10
+        if first_elf_start <= second_elf_start && first_elf_end >= second_elf_end && line != "" {
+            count += 1;
+            continue;
+        }
+        if first_elf_start >= second_elf_start && first_elf_end <= second_elf_end && line != "" {
+            count += 1;
+        }
+    }
+    println!("count: {}", count);
 }
